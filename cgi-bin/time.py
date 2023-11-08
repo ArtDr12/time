ce882341925629ef9daa94f3dc0e88274fcd28a3
ArtDr12 <141354887+ArtDr12@users.noreply.github.com>
2023-11-08 15:25:25 +0300

    Delete cgi-bin directory

--- cgi-bin/time.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-
-print("Content-Type: text/html")
-print()
-print(f"<H1>Current Time {current_time}</H1>")
